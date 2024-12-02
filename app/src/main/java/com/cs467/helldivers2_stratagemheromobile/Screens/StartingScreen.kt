84c7fe02492c965d4fec1b899bd1af57a0430fe0
SerAM90 <serranoam90@gmail.com>
2024-12-01 20:54:32 -0500

    super earth flag background svg
    svg file

--- app/src/main/java/com/cs467/helldivers2_stratagemheromobile/Screens/StartingScreen.kt
+++ app/src/main/java/com/cs467/helldivers2_stratagemheromobile/Screens/StartingScreen.kt
@@ -1,18 +1,29 @@
 package com.cs467.helldivers2_stratagemheromobile.Screens
 
 import android.media.MediaPlayer
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -28,36 +39,55 @@ fun StartingScreen(
     navController: NavController
 ) {
     val context = LocalContext.current
-    Surface(
-        onClick = {
-            navController.navigate("ready_screen")
-            var mediaPlayer = MediaPlayer.create(context, R.raw.start) // Add a sound file in 'res/raw'
-            mediaPlayer?.start()
-            mediaPlayer?.setOnCompletionListener {
-                it.reset()
-                it.release() // Release the MediaPlayer when the sound is done
+
+    // Top-level container
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .clickable {
+                navController.navigate("ready_screen")
+
+                // Play start sound
+                val mediaPlayer = MediaPlayer.create(context, R.raw.start)
+                mediaPlayer?.start()
+                mediaPlayer?.setOnCompletionListener {
+                    it.reset()
+                    it.release() // Release resources when done
+                }
             }
-        }
     ) {
+        // Background image
+        Image(
+            painter = painterResource(R.drawable.superearthbackground),
+            contentDescription = "Super Earth Logo",
+            modifier = Modifier
+                .fillMaxHeight(0.8f)
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillHeight,
+            colorFilter = ColorFilter.tint(Color.White),
+            alpha = 0.2f
+        )
+
+        // Title and Instructions
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-
             Text(
                 text = stringResource(R.string.title_start_screen),
                 fontSize = 50.sp,
+                color = Color.White
             )
+            Spacer(modifier = Modifier.height(16.dp)) // Space between texts
             Text(
-                text = stringResource(id = R.string.start_screen_instructions),
+                text = stringResource(R.string.start_screen_instructions),
                 fontSize = 30.sp,
                 color = Color.Yellow
             )
         }
     }
-
-
 }
 
 @Preview(showBackground = true, widthDp = 800, heightDp = 360)
